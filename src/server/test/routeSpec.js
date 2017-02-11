102ca733b707c183dd50e87dd20876fba2d85e1f
juliusbuckley <juliusbuckley@gmail.com>
2017-02-11 01:52:41 -0800

    Updated API test

--- src/server/test/routeSpec.js
+++ src/server/test/routeSpec.js
@@ -1,12 +1,22 @@
-import app from '../index.js';
+import app from '../app.js';
 import request from 'supertest';
 import { expect } from 'chai';
 
-describe('HTTP /endpoint', () => {
-  it('should return 200 to confirm request are sent to endpoint', done => {
+const search = '1770 Union St, San Francisco, CA 94115';
+const searchArray = search.split(',');
+const data = {
+  address: searchArray[0],
+  city: searchArray[1].substring(1),
+  state: searchArray[2].split(' ')[1],
+  zip: searchArray[2].split(' ')[2]
+};
+
+describe('POST /distance', () => {
+  it('should return 200 to confirm request are sent to distance', done => {
     request(app)
-      .get(/**/)
-      .query(/**/)
+      .post('/distance')
+      .send(data)
       .expect(200, done);
   });
 });
+   
